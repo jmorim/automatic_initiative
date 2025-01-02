d578d11da72c04fb0a08773992118fee25ad45f9
jmorim <jmorim@mit.edu>
2025-01-02 17:48:19 -0500

    forgot to commit module.js

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,6 @@
 const MODULE_ID = 'automatic-initiative'
 
 const dialogGenerator = async (combatant) => {
-  console.log('combatant at dialogGenerator: ', combatant)
   const initiativeDialog = new Dialog({
     title: 'roll for initiative',
     content: "<p>we don't got all day</p>",
@@ -25,17 +24,14 @@ const dialogGenerator = async (combatant) => {
 };
 
 const rollNPC = async (combatant) => {
-  console.log('combatant at rollNPC: ', combatant);
   try {
     const result =  await combatant.rollInitiative?.();
-    console.log(combatant.id, ' rolled ', result.initiative);
   } catch (error) {
     console.log('error rolling npc initiative: ', error)
   }
 };
 
 const rollPlayers = async (combatant) => {
-  console.log('combatant at rollPlayers: ', combatant);
   try {
     await dialogGenerator(combatant);
   } catch (error) {
@@ -43,33 +39,21 @@ const rollPlayers = async (combatant) => {
   }
 };
 
-let hookFire = 0;
 // When combatant created
 Hooks.on("createCombatant", async (combatant) => {
-  hookFire++;
-  console.log('hook fired this many times: ', hookFire)
-  console.log(MODULE_ID, ` | rolling initiative for ${combatant.name}`);
-  console.log(MODULE_ID, ` | combatant: `, combatant);
   // If combatant is NPC and doesn't have initiative
   if (combatant.isNPC == true && !combatant.initiative && game.user === game.users.activeGM) {
     // Roll initiative for NPC as the GM
-//    await socket.executeAsGM("rollNPC", combatant);
     rollNPC(combatant);
-//    rollNPC(combatant);
   } else if (combatant.actor?.hasPlayerOwner && !combatant.initiative) {
-    console.log('users: ', game.users);
-    console.log('current user: ', User.current);
     let player = game.users.players.find(
-      (p) => p.character?.id === combatant.actor.id
+      p => p.character?.id === combatant.actor.id
     );
-    console.log("player at createCombatant hook: ", player);
     if (player && player.active && player.id == game.userId) {
       try {
-//        rollPlayers(combatant);
-//        await socket.executeAsUser("rollPlayers", player.id, combatant);
         rollPlayers(combatant);
       } catch (error) {
-        console.log("error executing user socket: ", error);
+        console.log("error rolling for player: ", error);
       }
     }
   }
